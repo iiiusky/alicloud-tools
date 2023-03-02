937f73f73b2320f8e748354ddda104f21c7510d7
sky <sky@03sec.com>
2023-03-02 15:00:17 +0800

    更新版本

--- main.go
+++ main.go
@@ -5,7 +5,7 @@ Licensed under the Apache License, Version 2.0 (the "License");
 you may not use this file except in compliance with the License.
 You may obtain a copy of the License at
 
-    http://www.apache.org/licenses/LICENSE-2.0
+	http://www.apache.org/licenses/LICENSE-2.0
 
 Unless required by applicable law or agreed to in writing, software
 distributed under the License is distributed on an "AS IS" BASIS,
@@ -21,6 +21,6 @@ import (
 )
 
 func main() {
-	common.APPVersion = "1.0.4"
+	common.APPVersion = "1.0.5"
 	cmd.Execute()
 }
